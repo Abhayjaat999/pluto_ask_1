d7e0a453960f374b3857a434958bce3913f28790
Abhay999 <abhayjaat999@gmail.com>
2022-08-06 14:55:56 +0530

    assin 1

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -2,13 +2,14 @@ const trim=function(){
 const greeting = "    Function Up    ";
 console.log(greeting,'to==>' ,greeting.trim());
 }
+
 const lower=function(){
     const sentence = 'THIS LOWER CASE ';
 console.log('THIS LOWER CASE     to==> ',sentence.toLowerCase());
 }
+
 const upper=function(){
 const sentence1 = 'this upper case';
-
 console.log('this upper case     to==> ',sentence1.toUpperCase());
 }
 
