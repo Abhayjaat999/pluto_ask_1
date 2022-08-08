2094349238eccf3a202fa7f8e022ad69e8f526d5
Abhay999 <abhayjaat999@gmail.com>
2022-08-08 16:14:35 +0530

    class assin

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const abc = require('../introduction/intro')
-const ask = require('../logger/logger')
+const oneSymbol = require('../logger/logger')
 const ask1 = require('../util/helper')
 const ask2 = require('../validator/formatter')
 const router = express.Router();
+const lodash=require('lodash')
 
 router.get('/test-me', function (req, res) {
     // console.log('My batch is', abc.name)
     // abc.printName()
-    ask.time()
+    oneSymbol.time()
     ask1.month()
-    ask.intro()
+    oneSymbol.intro()
     ask1.InfoBatch()
     ask2.getTrim()
     ask2.lowerw()
     ask2.toUpper()
     
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    console.log(lodash.chunk(month,4));
+    const odd=[1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(odd,9))
+
     res.send('My second ever api!')
 });
 
+router.get('/student-detaile/:name',function(req,res){
+    console.log("this is the request "+JSON.stringify(req.params))
+    let reqParams=req.params
+    let studentName=reqParams.name
+    console.log();
+})
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
