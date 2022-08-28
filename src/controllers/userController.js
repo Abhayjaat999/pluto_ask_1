01da3afd506b86a1046f3af17d3f83c91e3d157b
Abhay999 <abhayjaat999@gmail.com>
2022-08-29 00:00:13 +0530

    Authentication with JWT

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,47 +1,75 @@
-const UserModel= require("../models/userModel")
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel');
 
+//=========================api-1========================================>>>
 
+const createUser = async function(req,res){
+    let data = req.body;
+    let user = await userModel.create(data);
+    res.send({data:user});
+}
 
+//=========================api-2========================================>>>
 
-const basicCode= async function(req, res, next) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    //res.send({ msg: "This is coming from controller (handler)"})
-    next()
+const loginUser = async function(req,res){
+    let email = req.body.emailId ;
+    let password = req.body.password ;
+    let login = await userModel.find({emailId: email, password: password});
+    if(!login){
+        return res.semd({msg : "Email & Password is not correct"});
     }
-
-const createUser= async function (req, res) {
-    
-    let data= req.body
-    let tokenDataInHeaders= req.headers.token
-    //Get all headers from request
-    console.log("Request headers before modificatiom",req.headers)
-    //Get a header from request
-    console.log(req.headers.batch)
-    console.log(req.headers["content-type"])
-    console.log(tokenDataInHeaders)
-    //Set a header in request
-    req.headers['month']='June' //req.headers.month = "June"
-
-    //Set an attribute in request object
-    req.anything = "everything"
-    
-    
-    console.log("Request headers after modificatiom",req.headers)
-    
-    //Set a header in response
-    res.header('year','2022')
-    res.send({msg: "Hi"})
+    let token = jwt.sign({
+        userId : login._id ,    //payload
+        batch : "Plutonium"
+    },"functionUp-Plutonium")   //secret key
+    res.setHeader("x-auth-token", token)
+    res.send({status:true, data :{ token : token}});
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+//=========================api-3========================================>>> 
+
+const getUser = async function(req,res){
+   
+    let userId= req.params.userId ;
+    let userDetails = await userModel.findById(userId);
+    res.send({status:true , data: userDetails});
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+//=========================api-4========================================>>>
+ 
+const updateUser = async function(req,res){
+    let userId = req.params.userId;
+    
+    let userDetails = await userModel.findById(userId);
+    if(!userDetails){
+        return res.send({status:false, msg:"no such user is exist"});
+    }
+    let  userData = req.body;
+    let updateUser = await userModel.findOneAndUpdate({_id: userId}, userData, {new :true});
+    res.send({status:true , data: updateUser});
+
+}
+
+//=========================api-5========================================>>>
+
+const deleteUser = async function(req,res){
+    let userId = req.params.userId;
+
+    let userDetails = await userModel.findById(userId);
+    if(!userDetails){
+        return res.send({status:false, msg:"no such user is exist"});
+    }
+    let userData = req.body ;
+    userData.isDeleted = true ;
+    let deleteUser = await userModel.updateOne({_id: userId}, userData, {new: true})
+    res.send({status : true , data : deleteUser}); 
+}
+
+//=======================================================================>>>
+
+module.exports.createUser = createUser;
+module.exports.loginUser = loginUser;
+module.exports.getUser = getUser;
+module.exports.updateUser = updateUser;
+module.exports.deleteUser = deleteUser;
+
