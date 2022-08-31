4d251e14ea7d8c812ed61da8dd17cc8dc74ec19e
Abhay999 <abhayjaat999@gmail.com>
2022-08-31 13:51:58 +0530

    Assinment Auth-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Abhay:5VFO0TgAXdoQcF0b@cluster0.dcwiqaa.mongodb.net/ASK-008?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
